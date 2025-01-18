6a6fe47d50127bd502ada398e1e3252a98f0bf4a
Mateus Melo <melo.mateus@proton.me>
2025-01-18 18:30:12 -0300

    Update font norse; adjust left image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
+      fontFamily: {
+        norse: ["Norse", "sans-serif"],
+      },
       extend: {},
     },
     plugins: [],
